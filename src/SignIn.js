058a4feb2153b6ee2f1484780e372d51ee42bed0
unknown <Gusabary@126.com>
2019-02-13 13:50:41 +0800

    feat: add pages SignIn and SignUp

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,15 +1,30 @@
 import React from 'react'
-import { Paper, withStyles } from '@material-ui/core';
-
+import { Paper, withStyles, Typography, Toolbar, TextField, Button } from '@material-ui/core';
+import AccountCircle from '@material-ui/icons/AccountCircle'
 
 const styles = theme => ({
     paper: {
-        paddingTop: theme.spacing.unit * 20,
-        paddingBottom: theme.spacing.unit*20,
-        paddingLeft: theme.spacing.unit * 5,
+        marginTop: theme.spacing.unit * 18,
+        marginRight: theme.spacing.unit * 55,
+        marginLeft: theme.spacing.unit * 55,
+        paddingTop: theme.spacing.unit * 5,
+        paddingBottom: theme.spacing.unit * 6,
+        //paddingLeft: theme.spacing.unit * 5,
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
+    },
+    text: {
+        paddingLeft: theme.spacing.unit * 2
+    },
+    textField: {
+        marginTop: theme.spacing.unit * 2,
+        marginLeft: -theme.spacing.unit * 5,
+        marginRight: -theme.spacing.unit * 5,
+        width: '70%',
+    },
+    button: {
+        marginTop: theme.spacing.unit * 7,
     },
 });
 
@@ -18,9 +33,33 @@ class SignIn extends React.Component {
         const { classes } = this.props;
         return (
             <React.Fragment>
-                <p>a</p>
+
                 <Paper className={classes.paper}>
-                    de
+                    <Toolbar>
+                        <AccountCircle fontSize="large" />
+                        <Typography variant="h4" className={classes.text} >
+                            Sign In
+                        </Typography>
+                    </Toolbar>
+                    <TextField
+                        type="email"
+                        label="Email Address"
+                        className={classes.textField}
+                        required
+                    />
+                    <TextField
+                        type="password"
+                        label="Password"
+                        className={classes.textField}
+                        required
+                    />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        className={classes.button}
+                    >
+                        Sign In
+                    </Button>
                 </Paper>
             </React.Fragment>
         )
---
+++ src/SignUp.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Paper, withStyles, Typography, Toolbar, TextField, Button } from '@material-ui/core';
+import AccountCircle from '@material-ui/icons/AccountCircle'
+
+const styles = theme => ({
+    paper: {
+        marginTop: theme.spacing.unit * 18,
+        marginRight: theme.spacing.unit * 55,
+        marginLeft: theme.spacing.unit * 55,
+        paddingTop: theme.spacing.unit * 5,
+        paddingBottom: theme.spacing.unit * 6,
+        //paddingLeft: theme.spacing.unit * 5,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    text: {
+        paddingLeft: theme.spacing.unit * 2
+    },
+    textField: {
+        marginTop: theme.spacing.unit * 2,
+        marginLeft: -theme.spacing.unit * 5,
+        marginRight: -theme.spacing.unit * 5,
+        width: '70%',
+    },
+    button: {
+        marginTop: theme.spacing.unit * 7,
+    },
+});
+
+class SignUp extends React.Component {
+    render() {
+        const { classes } = this.props;
+        return (
+            <React.Fragment>
+
+                <Paper className={classes.paper}>
+                    <Toolbar>
+                        <AccountCircle fontSize="large" />
+                        <Typography variant="h4" className={classes.text} >
+                            Sign Up
+                        </Typography>
+                    </Toolbar>
+                    <TextField
+                        type="email"
+                        label="Email Address"
+                        className={classes.textField}
+                        required
+                    />
+                    <TextField
+                        type="password"
+                        label="Password"
+                        className={classes.textField}
+                        required
+                    />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        className={classes.button}
+                    >
+                        <Typography>
+                            Sign Up
+                        </Typography>
+                    </Button>
+                </Paper>
+            </React.Fragment>
+        )
+    }
+}
+
+export default withStyles(styles)(SignUp);
